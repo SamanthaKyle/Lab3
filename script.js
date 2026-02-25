3d55a5a49fd94b1bb807089def6c935d443665cf
Samantha Kyle <samanthalkyle@gmail.com>
2026-02-25 18:11:16 -0500

    first set of files

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtLWt5bGUiLCJhIjoiY21rZTR3NW82MDNjazNscHdvZGRoNTJlYyJ9.gXrPIIVvGXk6SEwdzrbd1g';
+
+const map = new mapboxgl.Map({
+    container: '',
+    style: 'mapbox://styles/mapbox/standard',
+    center: [-79.380752, 43.660356], 
+    zoom: 10,
+})
